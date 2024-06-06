a0172f304124529fc0e61d11ec1e5753af5f5dd5
Victor Rodriguez <victordz.dev@gmail.com>
2024-06-06 15:08:19 -0300

    Corrigindo problema com a validação do login

--- scripts/loginScript.js
+++ scripts/loginScript.js
@@ -2,23 +2,23 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
       event.preventDefault();
   
       const username = document.getElementById('username').value;
-      const senha = document.getElementById('password').value;
+      const senha = document.getElementById('senha').value;
       const mensagemErro = document.getElementById('mensagemErro');
   
       const login = 'usuario';
       const senhaCadastrada = 'senha123';
   
       if (username === login && senha === senhaCadastrada) {
-          alert('Login bem-sucedido!');
-          // Redirecionar ou executar ação de sucesso
+        alert('Login bem-sucedido!');
+        document.getElementById('username').value = '';
+        document.getElementById('senha').value = '';
+        mensagemErro.textContent = '';
       } else {
-            alert('Usuário ou senha incorretos.');
-          mensagemErro.textContent = 'Usuário ou senha incorretos.';
-          mensagemErro.style.display = 'block';
-  
-          // Limpar campos e focar no campo de usuário
-          document.getElementById('username').value = '';
-          document.getElementById('password').value = '';
-          document.getElementById('username').focus();
+        alert('Usuário ou senha incorretos.');
+        mensagemErro.textContent = 'Usuário ou senha incorretos.';
+        mensagemErro.style.display = 'block';
+        document.getElementById('username').value = '';
+        document.getElementById('senha').value = '';
+        document.getElementById('username').focus();
       }
   });
