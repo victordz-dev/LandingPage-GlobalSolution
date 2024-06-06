d17536037920a788c1bb59b3d77fbb06218078c2
Victor Rodriguez <victordz.dev@gmail.com>
2024-06-06 18:08:49 -0300

    Melhorando o carrossel

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -7,13 +7,34 @@ menu.addEventListener('click', () => {
 })
 
 let currentIndex = 0;
+let slideInterval;
 
 function moveSlide(direction) {
     const slides = document.querySelectorAll('.carousel-item');
     const totalSlides = slides.length;
 
     slides[currentIndex].classList.remove('active');
+
     currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
+
     document.querySelector('.carousel-inner').style.transform = `translateX(-${currentIndex * 100}%)`;
+
     slides[currentIndex].classList.add('active');
 }
+
+function startCarousel() {
+    slideInterval = setInterval(() => {
+        moveSlide(1);
+    }, 3000);
+}
+
+function stopCarousel() {
+    clearInterval(slideInterval);
+}
+
+window.onload = startCarousel;
+
+document.querySelector('.carousel').addEventListener('mouseover', stopCarousel);
+
+document.querySelector('.carousel').addEventListener('mouseout', startCarousel);
+
